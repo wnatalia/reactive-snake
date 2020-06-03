2b6e2c53a791bd225f2a1bc9a3579ef479395ea5
Natalia Wirkijowska <wirkijowska.natalia@gmail.com>
2020-06-03 18:46:09 +0200

    Remove not used import

--- src/containers/Game/index.test.js
+++ src/containers/Game/index.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { mount, shallow } from 'enzyme';
+import { shallow } from 'enzyme';
 import { useSelector } from 'react-redux';
 import Game from './index';
 import initialState from '../../redux/initialState';
